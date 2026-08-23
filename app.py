4d3af9720efa8495d11f8635125caa7046260a1c
srushtithakre5-sys <srushtithakre5@gmail.com>
2026-08-23 14:25:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,41 @@ if uploaded_file is not None:
 
 else:
     st.warning("Please upload your resume.")
+    # SKILLS DETECTION
+
+skills = [
+    "python",
+    "java",
+    "c",
+    "c++",
+    "html",
+    "css",
+    "javascript",
+    "sql",
+    "mysql",
+    "linux",
+    "windows",
+    "ccna",
+    "networking",
+    "flask",
+    "machine learning",
+    "excel"
+]
+
+found_skills = []
+
+for skill in skills:
+    if skill.lower() in text.lower():
+        found_skills.append(skill)
+
+st.subheader("🛠️ Skills Detected")
+
+if found_skills:
+
+    for skill in found_skills:
+        st.write("✅", skill.title())
+
+else:
+
+    st.warning("No skills detected.")
     
